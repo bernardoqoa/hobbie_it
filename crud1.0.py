49a191c731a63d752399ab493cf6503924061ede
bernardoqoa <bqoa@cesar.school>
2020-11-28 16:51:49 -0300

    acrescentei coisas novas

--- crud1.0.py
+++ crud1.0.py
@@ -1,43 +1,28 @@
 from funcoes_usuarios import *
 
+
 def main():
+    opcoes = [1, 2, 3, 4, -1]
     opcao = 0
     while opcao != -1:
         print('** CRUD **')
-        print('1 - Cadastrar Usuário''\n''2 - Consultar Usuário''\n''3 - Editar Usuário''\n''4 - Apagar Usuário''\n''-1 - Encerrar')
+        print('1 - Cadastrar Usuário\n2 - Consultar Usuário\n3 - Editar Usuário\n4 - Apagar Usuário\n-1 - Encerrar')
         opcao = int(input('Opção desejada: '))
+        while opcao not in opcoes:
+            print("Você digitou uma opção invalida.")
+            opcao = int(input("Digite a opção desejada: "))
         if opcao == 1:
             cadastrar_usu()
         elif opcao == 2:
             cosultar_usu()
-        elif opcao == 3:
-            editar_usu()
-        elif opcao == 4:
-            apagar_usu()
-        elif opcao == -1:
-            print('Programa finalizado com sucesso!')
+#       elif opcao == 3:
+#           editar_usu()
+#       elif opcao == 4:
+#           apagar_usu()
+#       elif opcao == -1:
+#           print('Programa finalizado com sucesso!')
 
 
 # Programa principal
-if name == 'main':
+if __name__ == '__main__':
     main()
-all_data = []
-
-
-def add():
-    noms = input("Type in your nom: ")
-    nome_usu = input("Type in your nomes_usu: ")
-    email = input("Type in your email: ")
-    senha = input("Type in your senhas: ")
-    curso = input("Type in your curso: ")
-    telefone = input("Type in your telefone: ")
-    all_data.append(
-        {
-            "noms": noms,
-            "nome_asu": nome_usu,
-            "email": email,
-            "curso": curso,
-            "telefone": telefone,
-            "senha": senha,
-        }
-    )
---
+++ funcoes_usuarios.py
@@ -0,0 +1,60 @@
+interesses = ["JOGOS", "FILMES", "FUTEBOL", "MÚSICA"]
+
+
+def cadastrar_usu():
+    usu_info = {}
+    print('1. Cadastro:')
+    nome = input("Nome completo: ")
+    usu_info['nome'] = nome + ','
+    nome_usu = input("Nome de usuário: ") + ','
+    email = input("Email: ")
+    usu_info['email'] = email + ','
+    senha = input("Senha: ")
+    senha2 = input("Confirme sua senha:")
+    while senha != senha2:
+        senha2 = input('As senhas não coincidem! Digite novamente: ')
+    if senha == senha2:
+        usu_info['senha'] = senha + ','
+    curso = input("Curso: ")
+    usu_info['curso'] = curso + ','
+    telefone = input("Telefone: ")
+    usu_info['telefone'] = telefone + ','
+    print(interesses)
+    i = int(input('Quantos desses interesses você se interessa? '))
+    interesses_usu = []
+    for c in range(i):
+        interesse_usu = input('Digite um desses interesses: ').upper()
+        while interesse_usu not in interesses:
+            print('Você digitou um interesse indisponivel!')
+            print(f'Os interesses disponiveis são: {interesses}')
+            interesse_usu = input('Digite novamente: ').upper()
+        if c == (i-1):
+            interesse_usu = interesse_usu + '\n'
+        else:
+            interesse_usu = interesse_usu + ','
+        interesses_usu.append(interesse_usu)
+    usu_info['interesses'] = interesses_usu
+    usuarios = open('usuarios.txt', 'a', encoding='utf8')
+    usuarios.writelines(nome_usu)
+    for info in usu_info:
+        usuarios.writelines(usu_info[info])
+    usuarios.close()
+
+
+def cosultar_usu():
+    usuarios = open('usuarios.txt', 'r', encoding='utf8')
+    lista_usuarios = usuarios.readlines()
+    usu = input('Digite o nome de usuário:')
+    for x in range(len(lista_usuarios)):
+        usu_info = lista_usuarios[0].split(',')
+        if usu == usu_info[0]:
+            indice = x
+    print(indice)
+
+
+
+#def editar_usu():
+
+
+
+#def apagar_usu():
